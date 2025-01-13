2c90a7195b5d1c926cf376494669411e48cb4b1a
TomasReyzak <zaktimur2002@gmail.com>
2025-01-13 12:05:59 +0300

    Initial commit

---
+++ Second2/scripts/script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let user = prompt('Как тебя зовут?','Тони');
+alert(`Привет, ${user}!`);
+
+//alert(String.fromCharCode(63));
+// let isBoss = confirm("Ты учишься?")
+// alert(isBoss);
+// let apples = "2";
+// let oranges = "3";
+// alert( apples - oranges );
+// alert("Dragon's mother said \"No\"");
+// alert('Dragon' + 'stone');
+/* Cделать перевоl строки можно используя символ перевода строки: \n. */
