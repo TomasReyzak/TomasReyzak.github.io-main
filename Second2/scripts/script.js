460cfa64551b70be5fdfe4eddc647e8543678584
TomasReyzak <zaktimur2002@gmail.com>
2025-01-15 13:26:05 +0300

    Update script.js

--- Second2/scripts/script.js
+++ Second2/scripts/script.js
@@ -2,18 +2,30 @@
 
 
 
-//  let apples = "2";
-//  let oranges = "3";
-//  lert( +apples + +oranges );
-//  alert( +a );
-//  let user = prompt('Как тебя зовут?','Тони');
-//  alert(`Привет, ${user}!`);
-//  alert(String.fromCharCode(63));
-//  let isBoss = confirm("Ты учишься?");
-//  alert(isBoss);
-//  let apples = "2";
-//  let oranges = "3";
-//  alert( apples - oranges );
-//  alert("Dragon's mother said \"No\"");
-//  alert('Dragon' + 'stone');
-/*  Cделать перевод строки можно используя символ перевода строки: \n. */
+/* 
+const firstName = 'Joffrey';
+const greeting = 'Hello';
+console.log(`${greeting}, ${firstName}!`);
+
+let apples = "2";
+let oranges = "3";
+lert( +apples + +oranges );
+
+alert( +a );
+
+let user = prompt('Как тебя зовут?','Тони');
+alert(`Привет, ${user}!`);
+
+alert(String.fromCharCode(63));
+
+let isBoss = confirm("Ты учишься?");
+alert(isBoss);
+
+let apples = "2";
+let oranges = "3";
+alert( apples - oranges );
+
+alert("Dragon's mother said \"No\"");
+alert('Dragon' + 'stone');
+
+Cделать перевод строки можно используя символ перевода строки: \n. */
